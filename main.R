a7beab1fb63506ac8bff9dbe6bd923ae1e03b160
Peter Conrad <peter.conrad@uni-jena.de>
2019-07-24 12:45:48 +0200

    Cleanup

--- main.R
+++ main.R
@@ -1,98 +1,44 @@
 library(leaps)
 
 nir.data <- read.csv("NIR.csv", sep = ";", header=TRUE)
-head(nir.data)
-ncol(nir.data)
 
-is.data.frame(nir.data)
-
-#nir.model <- regsubsets(y=nir.data[,2], x=nir.data[,4:ncol(nir.data)], really.big = TRUE)
-
-selectFeatures <- function(m){ #m matrix mit allein einflussgroessen
-  resultDF <- data.frame(matrix(ncol=0, nrow=nrow(m)))
-  for(i in seq(1, ncol(m), 2)){ # Spaltenindex
-    if (i >= ncol(m)) {
-      resultDF <- cbind(resultDF, m[,i,drop=FALSE])
+selectFeatures <- function(df){ #df Data Frame mit allen Einflussgroessen
+  resultDF <- data.frame(matrix(ncol=0, nrow=nrow(df)))
+  for(i in seq(1, ncol(df), 2)){ # Spaltenindex
+    if (i >= ncol(df)) {
+      resultDF <- cbind(resultDF, df[,i,drop=FALSE])
       return(resultDF)
     }
     
-    difference <- abs(sum((m[i] - m[i+1])) / nrow(m))
+    difference <- abs(sum((df[i] - df[i+1])) / nrow(df))
     
     if(difference >= 0.001){
-      resultDF <- cbind(resultDF, m[,i:(i+1)])
+      resultDF <- cbind(resultDF, df[,i:(i+1)])
       
     }
     else{
-      resultDF <- cbind(resultDF, m[,i,drop=FALSE])
+      resultDF <- cbind(resultDF, df[,i,drop=FALSE])
     }
   }
   return(resultDF)
 }
 
-help <- selectFeatures(nir.data[,4:ncol(nir.data), drop=FALSE])
-ncol(help)
-head(help)
-help2 <- selectFeatures(help)
-ncol(help2)
-help3 <- selectFeatures(help2)
-ncol(help3)
-help4 <- selectFeatures(help3)
-ncol(help4)
-help5 <- selectFeatures(help4)
-ncol(help5)
-help6 <- selectFeatures(help5)
-ncol(help6)
-help7 <- selectFeatures(help6)
-ncol(help7)
-help8 <- selectFeatures(help7)
-ncol(help8)
+reducedFeatures <- selectFeatures(nir.data[,4:ncol(nir.data), drop=FALSE]) 
+for (i in 1:5) {
+  reducedFeatures <- selectFeatures(reducedFeatures)  
+}
 
+nir.model <- regsubsets(
+  y=nir.data[,2],
+  x=reducedFeatures,
+  nvmax=ncol(reducedFeatures)+1,
+  method="backward",
+  really.big = TRUE)
 
-nir.model <- regsubsets(y=nir.data[,2], x=help8, nvmax=ncol(help8)+1, method="backward", really.big = TRUE)
 subsetForModel <- which.min(summary(nir.model)$cp)
 subsetM <- summary(nir.model)$which
 
 sum(subsetM[32,])
 
-plot(nir.model, scale="r2")
-
 
 lm1 <- lm(y=nir.data[,2], x)
-  
-  
-  
-#second step
-
-variabilityWithMean <- function(m2){
-  resultMatrix <- matrix(nrow=nrow(m2), ncol=0)
-  for(i in 1:ncol(m2)){
-    innerDifference <- (max(m2[,i]) - min(m2[,i])) / mean(m2[,i])
-    if(innerDifference > 0.001){
-      resultMatrix <- cbind(resultMatrix, m2[,i])
-    }
-  }
-  return(resultMatrix)
-}
-
-variability <- function(m2){
-  resultMatrix <- matrix(nrow=nrow(m2), ncol=0)
-  for(i in 1:ncol(m2)){
-    innerDifference <- max(m2[,i]) - min(m2[,i])
-    if(innerDifference > 0.001){
-      resultMatrix <- cbind(resultMatrix, m2[,i])
-    }
-  }
-  return(resultMatrix)
-}
-
-help9 <- variabilityWithMean(help8)
-ncol(help9)
-help10 <- variabilityWithMean(help9)
-ncol(help10)
-
-help9 <- variability(help8)
-ncol(help9)
-help10 <- variability(help9)
-ncol(help10)
-
-
